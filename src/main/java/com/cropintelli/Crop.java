3e8cbe88febbdc94fd5dbe1af280c69108704ad1
Yamil Tejada <yamil.tejada9.pumacayo10@gmail.com>
2026-08-30 11:57:57 -0500

    feat(crop): add crop entity model

---
+++ src/main/java/com/cropintelli/Crop.java
@@ -0,0 +1,43 @@
+package com.cropintelli;
+
+public class Crop {
+
+    // Attributes
+
+    private String name;
+    private int age;
+
+    // Constructor
+
+    public Crop(String name, int age){
+
+        this.name = name;
+        this.age = age;
+    }
+
+    // Methods
+
+    public void infoCrop(){
+
+        System.out.println("Name: " + name + " - Age: " + age );
+    }
+
+    // Getters and Setters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
